1e3753826ce9bda2b2d25f250b32d7def0a636c6
GerardSeef <gerard666@gmail.com>
2022-09-29 18:58:59 -0300

    Add files via upload
    exercise 2.8 1/2

--- part2/phonebook/App.js
+++ part2/phonebook/App.js
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 export const App = () => {
   const [ person, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ])
-  const [ newName, setNewName ] = useState('')
+    { name: 'Arto Hellas',      number: '+543764485976' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
+  const [ newName, setNewName ] = useState('');
+  const [ newNumber, setNewNumber ] = useState(''); 
+  const [ filterWord, setFilterWord ] =useState('');
+ // const [ filteredPersons, setFilteredPersons ] = useState([]);
+  
 
   const addPerson = (event) => {
     event.preventDefault();
     const personObject = {      
-      name: newName
+      name: newName,
+      number: newNumber,
     }    
     if(person.find(e => e.name === newName)) {
       alert( ` ${newName} is already added to the Phonebook`)
@@ -18,32 +26,54 @@ export const App = () => {
     }
     setPersons(person.concat(personObject));
     setNewName('');
+    setNewNumber(''); 
   }
-
   const handlePersonChange = (event) => {
     setNewName(event.target.value);
   }
+  const handleNumberchange = (event) => {
+    setNewNumber(event.target.value);
+  }
+  
+  const handleFilterChange = (event) => {      
+   setFilterWord(event.target.value);
+  }
 
-  return(
+  
+  return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson} >
-        <div>
+      <br />
+      filter shown in <input onChange={handleFilterChange} />
+      <form>
+      <div>
           name: <input 
             value={newName}
-            onChange={handlePersonChange} />
+            onChange={handlePersonChange} />         
+        </div>        
+        <div>
+          <br />          
+          number: <input
+            value={newNumber}
+            onChange={handleNumberchange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
-      </form>     
+      </form>
       <h2>Numbers</h2>
       <div>
         <ul>
-          {person.map( person => <li key={person.name}>{person.name}</li>)}
+          { person
+              .filter((person) => {  
+                if(filterWord === '') return true;
+                return person.name.includes(filterWord);
+              })
+              .map( person => <li key={person.name}> {person.name} {person.number}</li>) }
         </ul>
-      </div>
+      </div>     
     </div>
   )
 }
 
+export default App
